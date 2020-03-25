79d6c582feb37bc6054e41216d439a28336380ab
guizimbr <guilherme.andretasantos@hotmail.com>
2020-03-24 22:52:47 -0300

    Back-end of aplication

---
+++ backend/src/comentarios.js
@@ -0,0 +1,20 @@
+/**
+ * Rota / Recurso => "('/')"
+ */
+
+/**
+ * Metodos HTTP
+ * 
+ * GET: Buscar/Listar uma informacao no back-end
+ * POST: Criar uma informacao no back-end
+ * PUT: Alterar uma informacao no back-end
+ * DELETE: Deletar uma informacao no back-end
+ */
+
+/**
+ * TIPOS DE PARAMETROS
+ *
+ * Query Params: Parametros nomeados enviados na rota apos "?" (Filtros, paginacao)
+ * Route Params: Parametros ultilizados para identificar recursos
+ * Request Body: Corpo da requisicao, ultilizado para criar ou alterar recursos
+ */
